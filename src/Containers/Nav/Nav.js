a22ef592ad9e1640ece90f914d76baa38b2b1356
Bensully17 <bensully513@gmail.com>
2018-05-21 23:34:22 -0600

    tower init

---
+++ src/Containers/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <a>Calendar</a>
+        <a>Baby Stores</a>
+        <h1>Pregnancy Tracker</h1>
+        <a>Forum</a>
+        <a>Home</a>
+      </div>
+    )
+  }
+}
